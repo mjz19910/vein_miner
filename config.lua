5cd996f649d2febd27150ea424ed0b82c5d122c7
mjz19910 <matthias291999@gmail.com>
2025-08-09 03:40:28 -0700

    .

--- config.lua
+++ config.lua
@@ -94,7 +94,7 @@ vein_miner.CFG = {
 	sticky_nodes = {
 		["mesecons_stickyblocks:sticky_block_all"] = true,
 	},
-	cardinal_dirs = {p(0, 1, 0), p(0, -1, 0), p(1, 0, 0), p(-1, 0, 0), p(0, 0, 1), p(0, 0, -1)},
+	cardinal_dirs = {p(1, 0, 0), p(-1, 0, 0), p(0, 1, 0), p(0, -1, 0), p(0, 0, 1), p(0, 0, -1)},
 }
 ---Insert all elements into `vein_miner.CFG.VEC_DIRS`
 ---@param src  Vector[]
