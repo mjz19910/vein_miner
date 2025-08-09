320d9631b3803490cb7822696b83c725eeaff82d
mjz19910 <matthias291999@gmail.com>
2025-08-09 02:40:03 -0700

    add spacers to types.lua

--- types.lua
+++ types.lua
@@ -11,6 +11,7 @@
 ---@field contains_item fun(self: InvRef, listname: string, stack: ItemStack): boolean
 ---@field get_list_names fun(self: InvRef): string[]
 ---@field set_location fun(self: InvRef, location: string)
+local inv = {}
 
 ---@class MetaDataRef
 ---@field get_string fun(self: MetaDataRef, key: string): string
@@ -23,6 +24,7 @@
 ---@field from_table fun(self: MetaDataRef, table: table)
 ---@field get_inventory fun(self: MetaDataRef): InvRef
 ---@field set_inventory fun(self: MetaDataRef, inv: InvRef)
+local meta = {}
 
 ---@class ItemStack
 ---@field get_count fun(self: ItemStack): integer
@@ -33,6 +35,7 @@
 ---@field set_wear fun(self: ItemStack, wear: integer)
 ---@field get_meta fun(self: ItemStack): MetaDataRef
 ---@field to_string fun(self: ItemStack): string
+local stack = {}
 
 ---@class Player
 ---@field get_player_name fun(self: Player): string
@@ -44,14 +47,18 @@
 ---@field get_wielded_item fun(self: Player): ItemStack
 ---@field set_wielded_item fun(self: Player, item: ItemStack|string)
 ---@field is_player fun(self: any): boolean
+local player = {}
 
 ---@class Chunk
 ---@field area VoxelArea
 ---@field start_list Vector[]
+local chunk = {}
 
 ---@class VeinMinerConfig
 ---@field cardinal_dirs Vector[]
 ---@field MINE_ONLY_CUR_SET string[]
+local config = {}
 
 ---@class VeinMinerGlobal
 ---@field CFG VeinMinerConfig
+local vein_miner = {}
