84e50f65338df005377fff91ee33ecb8ee8a4745
mjz19910 <matthias291999@gmail.com>
2025-08-09 21:17:44 -0700

    add ObjectRef types

--- types.lua
+++ types.lua
@@ -75,36 +75,6 @@ local config = {}
 ---@field mine_only_cur_set table<string, boolean>
 ---@field mine_only_group_sets table<string, string>
 local vein_miner = {}
----@class ObjectRef
---- Position & Movement
----@field get_pos fun(self:ObjectRef): Vector
----@field set_pos fun(self:ObjectRef, pos:Vector)
----@field move_to fun(self:ObjectRef, pos:Vector, continuous?:boolean)
----@field get_velocity fun(self:ObjectRef): Vector
----@field add_velocity fun(self:ObjectRef, vel:Vector)
----@field get_acceleration fun(self:ObjectRef): Vector
----@field set_acceleration fun(self:ObjectRef, acc:Vector)
----@field get_rotation fun(self:ObjectRef): Vector
----@field set_rotation fun(self:ObjectRef, rot:Vector)
----@field get_yaw fun(self:ObjectRef): number
----@field set_yaw fun(self:ObjectRef, yaw:number)
----
---- Appearance
----@field set_texture_mod fun(self:ObjectRef, mod:string)
----@field get_texture_mod fun(self:ObjectRef): string
----@field set_sprite fun(self:ObjectRef, start_frame:number, num_frames:number, framelength:number, select_horiz_by_yawpitch:boolean)
----
---- Entity Properties
----@field get_properties fun(self:ObjectRef): table
----@field set_properties fun(self:ObjectRef, props:table)
----
---- Player Specific (only if this is a player)
----@field get_player_name fun(self:ObjectRef): string
----@field is_player fun(self:ObjectRef): boolean
----
---- Sound
----@field sound_play fun(self:ObjectRef, spec:SimpleSoundSpec, params?:ServerSoundParams, ephemeral?:boolean): integer|nil
-
 ---@class SimpleSoundSpec
 ---@field name string|string[] Sound name or list of names to choose from
 ---@field gain number|nil Default gain (volume multiplier)
@@ -216,3 +186,150 @@ local config_data = {}
 ---@field save_player_config fun(name: string): nil
 ---@field load_player_config fun(name: string): nil
 local p_config = {}
+
+---@class ObjectRef
+---
+---## Lifecycle
+---@field remove fun(self:ObjectRef)
+---@field is_valid fun(self:ObjectRef): boolean
+---@field get_guid fun(self:ObjectRef): string
+---
+---## Position & Movement
+---@field get_pos fun(self:ObjectRef): Vector
+---@field set_pos fun(self:ObjectRef, pos:Vector)
+---@field add_pos fun(self:ObjectRef, pos:Vector)
+---@field move_to fun(self:ObjectRef, pos:Vector, continuous?:boolean)
+---@field get_velocity fun(self:ObjectRef): Vector
+---@field set_velocity fun(self:ObjectRef, vel:Vector)
+---@field add_velocity fun(self:ObjectRef, vel:Vector)
+---@field get_acceleration fun(self:ObjectRef): Vector
+---@field set_acceleration fun(self:ObjectRef, acc:Vector)
+---@field get_rotation fun(self:ObjectRef): Vector
+---@field set_rotation fun(self:ObjectRef, rot:Vector)
+---@field get_yaw fun(self:ObjectRef): number
+---@field set_yaw fun(self:ObjectRef, yaw:number)
+---
+---## Interaction
+---@field punch fun(self:ObjectRef, puncher:ObjectRef, time_from_last_punch:number, tool_capabilities:ToolCaps, dir:Vector)
+---@field right_click fun(self:ObjectRef, clicker:ObjectRef)
+---
+---## Health
+---@field set_hp fun(self:ObjectRef, hp:number, reason?:table)
+---@field get_hp fun(self:ObjectRef): number
+---
+---## Inventory
+---@field get_inventory fun(self:ObjectRef): InvRef
+---@field get_wield_list fun(self:ObjectRef): string
+---@field get_wield_index fun(self:ObjectRef): number
+---@field get_wielded_item fun(self:ObjectRef): ItemStack
+---@field set_wielded_item fun(self:ObjectRef, item:ItemStack)
+---
+---## Armor
+---@field set_armor_groups fun(self:ObjectRef, groups:table<string,integer>)
+---@field get_armor_groups fun(self:ObjectRef): table<string,integer>
+---
+---## Physics
+---@field set_physics_override fun(self:ObjectRef, override_table:PhysicsOverride)
+---@field get_physics_override fun(self:ObjectRef): PhysicsOverride
+---
+---## Animation
+---@field set_animation fun(self:ObjectRef, frame_range?:Vector, frame_speed?:number, frame_blend?:number, frame_loop?:boolean)
+---@field set_animation_frame_speed fun(self:ObjectRef, frame_speed:number)
+---@field get_animation fun(self:ObjectRef): table
+---
+---## Bones
+---@field set_bone_position fun(self:ObjectRef, bone:string, position:Vector, rotation:Vector)
+---@field get_bone_position fun(self:ObjectRef, bone:string): Vector, Vector
+---@field set_bone_override fun(self:ObjectRef, bone:string)
+---@field get_bone_override fun(self:ObjectRef, bone:string): any
+---@field get_bone_overrides fun(self:ObjectRef): any
+---
+---## Attachment
+---@field set_attach fun(self:ObjectRef, parent:ObjectRef, bone?:string, position?:Vector, rotation?:Vector)
+---@field get_attach fun(self:ObjectRef): table
+---@field get_children fun(self:ObjectRef): ObjectRef[]
+---@field set_detach fun(self:ObjectRef)
+---
+---## Entity Properties
+---@field get_properties fun(self:ObjectRef): table
+---@field set_properties fun(self:ObjectRef, props:table)
+---
+---## Observers
+---@field set_observers fun(self:ObjectRef, observers:table)
+---@field get_observers fun(self:ObjectRef): table
+---@field get_effective_observers fun(self:ObjectRef): table
+---
+---## Identification
+---@field is_player fun(self:ObjectRef): boolean
+---
+---## Appearance
+---@field set_texture_mod fun(self:ObjectRef, mod:string)
+---@field get_texture_mod fun(self:ObjectRef): string
+---@field set_sprite fun(self:ObjectRef, start_frame:number, num_frames:number, framelength:number, select_horiz_by_yawpitch?:boolean)
+---
+---## Player Specific
+---@field get_player_name fun(self:ObjectRef): string
+---@field get_fov fun(self:ObjectRef): number
+---@field set_fov fun(self:ObjectRef, degrees:number, is_multiplier?:boolean, transition_time?:number)
+---@field get_look_dir fun(self:ObjectRef): Vector
+---@field get_look_vertical fun(self:ObjectRef): number
+---@field set_look_vertical fun(self:ObjectRef, radians:number)
+---@field get_look_horizontal fun(self:ObjectRef): number
+---@field set_look_horizontal fun(self:ObjectRef, radians:number)
+---@field set_breath fun(self:ObjectRef, breath:number)
+---@field get_breath fun(self:ObjectRef): number
+---@field get_meta fun(self:ObjectRef): MetaRef
+---@field set_inventory_formspec fun(self:ObjectRef, formspec:string)
+---@field get_inventory_formspec fun(self:ObjectRef): string
+---@field set_formspec_prepend fun(self:ObjectRef, formspec:string)
+---@field get_formspec_prepend fun(self:ObjectRef): string
+---@field get_player_control fun(self:ObjectRef): table
+---@field get_player_control_bits fun(self:ObjectRef): integer
+---
+---## HUD
+---@field hud_add fun(self:ObjectRef, form:HUDDef): integer
+---@field hud_remove fun(self:ObjectRef, id:integer)
+---@field hud_change fun(self:ObjectRef, id:integer, stat:string, data:any)
+---@field hud_get_next_id fun(self:ObjectRef): integer
+---@field hud_get fun(self:ObjectRef, id:integer): HUDDef
+---@field hud_get_all fun(self:ObjectRef): table<integer,HUDDef>
+---@field hud_set_flags fun(self:ObjectRef, flags:HUDFlags)
+---@field hud_get_flags fun(self:ObjectRef): HUDFlags
+---@field hud_set_hotbar_itemcount fun(self:ObjectRef, count:integer)
+---@field hud_get_hotbar_itemcount fun(self:ObjectRef): integer
+---@field hud_set_hotbar_image fun(self:ObjectRef, name:string)
+---@field hud_get_hotbar_image fun(self:ObjectRef): string
+---@field hud_set_hotbar_selected_image fun(self:ObjectRef, name:string)
+---@field hud_get_hotbar_selected_image fun(self:ObjectRef): string
+---
+---## Environment Visuals
+---@field set_sky fun(self:ObjectRef, sky_parameters:SkyParams)
+---@field get_sky fun(self:ObjectRef, as_table?:boolean): table|string
+---@field set_sun fun(self:ObjectRef, sun_parameters:SunParams)
+---@field get_sun fun(self:ObjectRef): SunParams
+---@field set_moon fun(self:ObjectRef, moon_parameters:MoonParams)
+---@field get_moon fun(self:ObjectRef): MoonParams
+---@field set_stars fun(self:ObjectRef, star_parameters:StarParams)
+---@field get_stars fun(self:ObjectRef): StarParams
+---@field set_clouds fun(self:ObjectRef, cloud_parameters:CloudParams)
+---@field get_clouds fun(self:ObjectRef): CloudParams
+---@field override_day_night_ratio fun(self:ObjectRef, ratio:number)
+---@field get_day_night_ratio fun(self:ObjectRef): number
+---@field set_local_animation fun(self:ObjectRef, idle:table, walk:table, dig:table, walk_while_dig:table, frame_speed:number)
+---@field get_local_animation fun(self:ObjectRef): table
+---@field set_eye_offset fun(self:ObjectRef, firstperson:Vector, thirdperson:Vector, thirdperson_front:Vector)
+---@field get_eye_offset fun(self:ObjectRef): Vector, Vector, Vector
+---@field set_camera fun(self:ObjectRef, params:CameraParams)
+---@field get_camera fun(self:ObjectRef): CameraParams
+---@field set_nametag_attributes fun(self:ObjectRef, attributes:table)
+---@field get_nametag_attributes fun(self:ObjectRef): table
+---@field send_mapblock fun(self:ObjectRef, pos:Vector)
+---@field set_minimap_modes fun(self:ObjectRef, modes:table, wanted_mode:integer)
+---@field set_lighting fun(self:ObjectRef, lighting:LightingParams)
+---@field get_lighting fun(self:ObjectRef): LightingParams
+---@field respawn fun(self:ObjectRef)
+---@field set_flags fun(self:ObjectRef, flags:EntityFlags)
+---@field get_flags fun(self:ObjectRef): EntityFlags
+
+---@type ObjectRef
+local ObjectRef = {}
