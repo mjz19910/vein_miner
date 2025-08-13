48fd8dd5527b089961f220f05150e56b65640d28
mjz19910 <matthias291999@gmail.com>
2025-08-13 02:26:14 -0700

    .

--- auto_floor.lua
+++ auto_floor.lua
@@ -212,7 +212,7 @@ core.register_globalstep(function(dtime)
 			time_until_block_place = 0
 		end
 		if j <= 1 and not is_yaw_update_per_player_disabled[name] then
-			local yaw_max = 0.3
+			local yaw_max = math.pow(9 / 10, 12)
 			local yaw_speed = yaw_max * dbg_count / 2
 			local new_yaw = math.rad((math.deg(yaw) + yaw_speed) % 360)
 			if last_yaw_per_player[name] and new_yaw + 0.1 < last_yaw_per_player[name] then
