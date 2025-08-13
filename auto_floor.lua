7fee02fff7a2a39035fc9543ad5d97110eed7436
mjz19910 <matthias291999@gmail.com>
2025-08-12 23:38:52 -0700

    .

--- auto_floor.lua
+++ auto_floor.lua
@@ -204,7 +204,7 @@ core.register_globalstep(function(dtime)
 		end
 		if j <= 1 and not is_yaw_update_per_player_disabled[name] then
 			local yaw_max = 0.1
-			local yaw_speed = yaw_max * dbg_count / 8
+			local yaw_speed = yaw_max * dbg_count / 9
 			local new_yaw = math.rad((math.deg(yaw) + yaw_speed) % 360)
 			player:set_look_horizontal(new_yaw)
 			dbg_count = dbg_count + 1
@@ -217,7 +217,7 @@ core.register_globalstep(function(dtime)
 				goto continue
 			end
 		else
-			dbg_count = math.floor(dbg_count / (math.log(dbg_count + 1, 2) + 2))
+			dbg_count = math.floor(dbg_count / (math.log(dbg_count + 1, 2) * 2 + 3))
 		end
 		last_yaw_per_player[name] = yaw
 		::continue::
