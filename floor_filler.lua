4d190cf17f39672e337f1e819a7bd2eb511cc127
mjz19910 <matthias291999@gmail.com>
2025-08-18 08:17:33 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -424,7 +424,7 @@ function FloorScanState:_maybe_update_yaw(player, yaw, ctrl)
 	else
 		yaw_speed = yaw_for_arc(LINE_LENGTH)
 	end
-	yaw_speed = yaw_speed / 1.3
+	yaw_speed = yaw_speed / 2
 	local new_yaw
 	if ctrl.sneak then
 		new_yaw = self.scan_radians - yaw_speed
