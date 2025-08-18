da536e389d3f3f3f84586aa0a737bbea677ba810
mjz19910 <matthias291999@gmail.com>
2025-08-18 07:44:11 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -430,7 +430,7 @@ function FloorScanState:_maybe_update_yaw(player, yaw, ctrl)
 	end
 	local yaw_speed
 	if self.max_dist then
-		yaw_speed = yaw_for_arc(self.max_dist + 16)
+		yaw_speed = yaw_for_arc(math.min(self.max_dist + 16, LINE_LENGTH + 1))
 	else
 		yaw_speed = yaw_for_arc(LINE_LENGTH + 1)
 	end
