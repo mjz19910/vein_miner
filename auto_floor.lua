0ba24f748c5aaf2500323d3a0caed539595eb547
mjz19910 <matthias291999@gmail.com>
2025-08-18 23:08:13 -0700

    .

--- auto_floor.lua
+++ auto_floor.lua
@@ -1,38 +1,11 @@
 local require = require
-local print = print
-local setmetatable = setmetatable
-local tostring = tostring
-local type = type
 ---@type LuantiCore
 local core = core
 
-local assert = assert
 ---@type VectorModule
-local vector = vector
 local ipairs = ipairs
-local pairs = pairs
-local table = table
-local math = math
----@type VeinMinerGlobal
-local vein_miner = vein_miner
 
--- localize math
-local round = vector.round
-local offset = vector.offset
-local normalize = vector.normalize
-local multiply = vector.multiply
-local new_vec = vector.new
-
-local set_node = core.set_node
-local get_node = core.get_node
-local get_node_or_nil = core.get_node_or_nil
-local sound_play = core.sound_play
 local get_connected_players = core.get_connected_players
-
-local registered_nodes = core.registered_nodes
-
-local player_config_mgr = vein_miner.player_config_mgr
-assert(player_config_mgr, "need vein_miner.player_config_mgr")
 
 core.register_tool("vein_miner:auto_floor", {
 	description = "Auto-Floor Builder",
@@ -67,9 +40,7 @@ core.register_globalstep(function(dtime)
 	core.is_async = nil
 end)
 
-local auto_floor_recipe = {{"default:stick", "", "default:stick"}, {"", "default:cobble", ""}, {"", "default:mese_crystal_fragment", ""}};
-
 core.register_craft({
 	output = "vein_miner:auto_floor",
-	recipe = auto_floor_recipe,
+	recipe = {{"default:stick", "", "default:stick"}, {"", "default:cobble", ""}, {"", "default:mese_crystal_fragment", ""}},
 })
