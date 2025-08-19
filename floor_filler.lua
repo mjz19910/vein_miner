a498ca31374df30cce9940414c4d409cf29339c9
mjz19910 <matthias291999@gmail.com>
2025-08-19 11:44:28 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -527,7 +527,7 @@ function FloorScanState:_maybe_update_yaw(player, yaw, ctrl)
 	end
 	local dist
 	if self.max_dist then dist = self.max_dist end
-	local yaw_speed = get_yaw_speed_for_distance(dist or self.place_limit) / 1.8
+	local yaw_speed = get_yaw_speed_for_distance(dist or self.place_limit) / 1.9
 	if ctrl.sneak then yaw_speed = -yaw_speed end
 	self.scan_radians = self.scan_radians + yaw_speed -- * math.log((self.count / 5) + 0.2, 3.5)
 	self.count = self.count + 1
