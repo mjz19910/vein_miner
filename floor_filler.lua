564e01d5589cc8e543d25dadc123274dc031683b
mjz19910 <matthias291999@gmail.com>
2025-08-18 21:46:37 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -130,7 +130,7 @@ local DISTANCE_INCREASE = 8
 ---@field blocks_this_tick integer Number of blocks placed this tick
 local FloorScanState = {
 	-- player yaw constants
-	target_rad = math.rad(360) * 2,
+	target_rad = math.rad(360),
 }
 
 ---@param self FloorScanState
