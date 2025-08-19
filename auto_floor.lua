b76ef9577c810281e8eb27060dd7c843c5eed4dc
mjz19910 <matthias291999@gmail.com>
2025-08-18 23:08:54 -0700

    .

--- auto_floor.lua
+++ auto_floor.lua
@@ -1,33 +1,21 @@
 local require = require
 ---@type LuantiCore
 local core = core
-
 ---@type VectorModule
 local ipairs = ipairs
-
 local get_connected_players = core.get_connected_players
-
-core.register_tool("vein_miner:auto_floor", {
-	description = "Auto-Floor Builder",
-	inventory_image = "default_wood.png",
-})
-
 ---@type FloorFiller
 local floor_filler = require("mods.vein_miner.floor_filler")
-
 ---@type table<string, FloorScanState>
 local scan_state_map = {}
-
 core.register_on_joinplayer(function(player)
 	local player_name = player:get_player_name()
 	scan_state_map[player_name] = floor_filler.new(player)
 end)
-
 core.register_on_leaveplayer(function(player)
 	local player_name = player:get_player_name()
 	scan_state_map[player_name] = nil
 end)
-
 -- globalstep for vein_miner:auto_floor tool
 core.register_globalstep(function(dtime)
 	core.is_async = true
@@ -40,6 +28,10 @@ core.register_globalstep(function(dtime)
 	core.is_async = nil
 end)
 
+core.register_tool("vein_miner:auto_floor", {
+	description = "Auto-Floor Builder",
+	inventory_image = "default_wood.png",
+})
 core.register_craft({
 	output = "vein_miner:auto_floor",
 	recipe = {{"default:stick", "", "default:stick"}, {"", "default:cobble", ""}, {"", "default:mese_crystal_fragment", ""}},
