3b8d129ad085b6c1c041b287de270241841cd1f8
mjz19910 <matthias291999@gmail.com>
2025-08-19 03:05:40 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -490,17 +490,20 @@ function FloorScanState:get_angle_deg() return rad_to_deg_wrap360(self.scan_radi
 ---@param yaw number Current yaw
 ---@param ctrl table Player control state
 function FloorScanState:_maybe_update_yaw(player, yaw, ctrl)
-	if self.blocks_this_tick ~= 0 or self.yaw_update_disabled then return end
+	if self.yaw_update_disabled then return end
+	if self.undo_last_yaw_step and self.blocks_this_tick > 0 then
+		local dist
+		if self.max_dist then dist = self.max_dist + 8 end
+		local yaw_speed = get_yaw_speed_for_distance(dist or self.place_limit) / 2
+		self.scan_radians = self.scan_radians - yaw_speed - yaw_speed * math.log(self.count + 1, 1.9) / 1.5
+		self.undo_last_yaw_step = false;
+	end
+	if self.blocks_this_tick ~= 0 then return end
 	local dist
 	if self.max_dist then dist = self.max_dist + 8 end
 	local yaw_speed = get_yaw_speed_for_distance(dist or self.place_limit) / 2.5
 	if ctrl.sneak then yaw_speed = -yaw_speed end
-	if self.undo_last_yaw_step then
-		self.scan_radians = self.scan_radians - yaw_speed - yaw_speed * math.log(self.count + 1, 1.9) / 1.5
-		self.undo_last_yaw_step = false;
-	else
-		self.scan_radians = self.scan_radians + yaw_speed + yaw_speed * math.log(self.count + 1, 1.9) / 2.2
-	end
+	self.scan_radians = self.scan_radians + yaw_speed + yaw_speed * math.log(self.count + 1, 1.9) / 2.2
 	self.count = self.count + 1
 
 	-- Reset if scan exceeds full rotation
