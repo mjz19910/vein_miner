bd50d15b219bdc3791c1ee03acb8761b1c283839
mjz19910 <matthias291999@gmail.com>
2025-08-19 16:25:07 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -341,7 +341,7 @@ function FloorScanState:deactivate_tool()
 	end
 	if self.all_blocks_placed > 0 then self.all_blocks_placed = 0 end
 	if self.tool_active then
-		self.player:set_fov(0, false, 0)
+		-- self.player:set_fov(0, false, 0)
 		self.tool_active = false
 	end
 end
@@ -511,7 +511,7 @@ function FloorScanState:get_angle_deg() return rad_to_deg_wrap360(self.scan_radi
 function FloorScanState:_maybe_update_yaw(player, yaw, ctrl)
 	if self.yaw_update_disabled or self.blocks_this_tick ~= 0 then return end
 	if not self.tool_active then
-		player:set_fov(10, false, 0)
+		-- player:set_fov(10, false, 0)
 		self.tool_active = true
 	end
 	local yaw_speed = get_yaw_speed_for_distance(self:get_place_limit()) / 2.1
