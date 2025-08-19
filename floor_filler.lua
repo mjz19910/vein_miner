9f2b32e436d3e3a202aee940aad13ecb1a7bf207
mjz19910 <matthias291999@gmail.com>
2025-08-19 05:56:54 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -479,7 +479,7 @@ end
 
 local function get_yaw_speed_for_distance(dist)
 	local xz_offset = dist + 1
-	local max_dist = vector.new(xz_offset, 2.5, xz_offset):length()
+	local max_dist = vector.new(xz_offset, 0, xz_offset):length()
 	return yaw_for_arc(math.ceil(max_dist))
 end
 
