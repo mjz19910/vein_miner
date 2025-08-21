3aba6231dde0c9d5b75ceba0470df8a508a3a3fe
mjz19910 <matthias291999@gmail.com>
2025-08-21 00:51:36 -0700

    .

--- freeze_mapgen.lua
+++ freeze_mapgen.lua
@@ -10,3 +10,13 @@ core.register_chatcommand("freeze_mapgen", {
 		return true, "Mapgen disabled for you."
 	end,
 })
+
+core.register_chatcommand("resume_mapgen", {
+	params = "",
+	description = "Enable mapgen around you",
+	func = function(name)
+		local player = core.get_player_by_name(name)
+		player:set_mapgen_disabled(false)
+		return true, "Mapgen enabled for you."
+	end,
+})
--- src/mod.lua
+++ src/mod.lua
@@ -1,2 +1,3 @@
 local require = require
 require("mods.vein_miner.src.examples")
+require("mods.vein_miner.freeze_mapgen")
