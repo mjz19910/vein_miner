e61df5c637bfd637711247e733798b02ffb754d0
mjz19910 <matthias291999@gmail.com>
2025-08-20 22:20:50 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -573,7 +573,7 @@ function FloorScanState:_maybe_update_yaw(player, ctrl)
 		player:set_fov(10, false, 0)
 		self.tool_active = true
 	end
-	local yaw_speed = get_yaw_speed_for_distance(self:get_place_limit())
+	local yaw_speed = get_yaw_speed_for_distance(self:get_place_limit()) / 1.25
 	if ctrl.sneak then yaw_speed = -yaw_speed end
 
 	-- -- detect crossing 0° in either direction
