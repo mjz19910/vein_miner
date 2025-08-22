5c8c902eee31b6eed003ae6a5a04a3510c86017b
mjz19910 <matthias291999@gmail.com>
2025-08-22 03:17:43 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -567,14 +567,9 @@ function FloorScanState:main_loop(player, placeable_node_name)
 
 			if prev_sector ~= curr_sector then
 				self:_reset_range_vars()
-				if curr_sector == 0 then
-					core.log("action", "two full rotations, moving down")
-					core.log("action", "avg " .. self.nodes_per_tick_avg)
-					self.player_pos = safe_set_player_pos(player, vector.offset(self.player_pos, 0, -1, 0))
-					self.line_start = vector.offset(self.player_pos, 0, -1, 0)
-				else
-					core.log("action", ("full rotation %d"):format(curr_sector))
-				end
+				core.log("action", "full rotation, moving down")
+				self.player_pos = safe_set_player_pos(player, vector.offset(self.player_pos, 0, -1, 0))
+				self.line_start = vector.offset(self.player_pos, 0, -1, 0)
 			end
 		else
 			self.current_yaw_rad = player:get_look_horizontal() + math.pi / 2
