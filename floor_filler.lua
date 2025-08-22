09321ab0becd3391ddcccd9204bebf84535db119
mjz19910 <matthias291999@gmail.com>
2025-08-22 00:26:30 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -559,7 +559,7 @@ function FloorScanState:main_loop(player, placeable_node_name)
 	end
 	if self.nodes_this_loop > 0 then self.nodes_this_tick = self.nodes_this_tick + self.nodes_this_loop end
 	if last_place_pos ~= nil and not self.yaw_update_disabled then player:set_pos(last_place_pos) end
-	if last_place_pos ~= nil and self.yaw_update_disabled and self.nodes_last_tick < 0.2 then
+	if last_place_pos ~= nil and self.yaw_update_disabled and math.floor(self.nodes_last_tick * 10000) == 0 then
 		player:set_pos(last_place_pos)
 	end
 	-- Handle yaw rotation if few blocks placed
