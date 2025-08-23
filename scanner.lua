7837c0d6701203e06c89448a09dfa35457489325
mjz19910 <matthias291999@gmail.com>
2025-08-23 02:58:34 -0700

    .

--- scanner.lua
+++ scanner.lua
@@ -212,16 +212,16 @@ function scanner.scan_nearby_lights(state, pos, node_name, options, show_log)
 		local size_change = light_scan_distance
 		if size_change < 1 then size_change = 1 end
 		if size.x > size_change * 2 then
-			r.min.x = r.min.x + math.floor(size_change / 2) + 2
-			r.max.x = r.max.x - math.floor(size_change / 2) - 2
+			r.min.x = r.min.x + math.floor(size_change / 1.5) + 1
+			r.max.x = r.max.x - math.floor(size_change / 1.5) - 1
 		end
 		if size.y > size_change * 2 then
-			r.min.y = r.min.y + math.floor(size_change / 2) + 2
-			r.max.y = r.max.y - math.floor(size_change / 2) - 2
+			r.min.y = r.min.y + math.floor(size_change / 1.5) + 1
+			r.max.y = r.max.y - math.floor(size_change / 1.5) - 1
 		end
 		if size.z > size_change * 2 then
-			r.min.z = r.min.z + math.floor(size_change / 2) + 2
-			r.max.z = r.max.z - math.floor(size_change / 2) - 2
+			r.min.z = r.min.z + math.floor(size_change / 1.5) + 1
+			r.max.z = r.max.z - math.floor(size_change / 1.5) - 1
 		end
 	end
 	if newly_scanned and player_config_mgr:is_light_debug_enabled(name) then for _, region in ipairs(regions) do region:draw() end end
