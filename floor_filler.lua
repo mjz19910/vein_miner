179b9a2c9bb3f54735259352e15211eee0ab2b3f
mjz19910 <matthias291999@gmail.com>
2025-08-25 09:31:21 -0700

    .

--- floor_filler.lua
+++ floor_filler.lua
@@ -685,6 +685,7 @@ function FloorScanState:main_loop(player, placeable_node_name)
 		if not try_place_block_from_inventory(player, playing_sounds, target_pos, place_limit) then goto next end
 		self:on_node_placed(target_pos, cur_len)
 		self.break_on_next = false
+		break
 		::next::
 	end
 	if self.nodes_this_loop > 0 then self.nodes_this_tick = self.nodes_this_tick + self.nodes_this_loop end
